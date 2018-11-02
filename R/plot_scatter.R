8618206e00f50a24ba398f80c9289239eeca0a86
andrew <36473866+andrew-MET@users.noreply.github.com>
2018-11-02 15:02:15 +0000

    Scatter plots

---
+++ R/plot_scatter.R
@@ -0,0 +1,78 @@
+#' Scatter plot of forecast vs observations
+#'
+#' @param .fcst A \code{harp_fcst} type object.
+#' @param fcst_model The model in the \code{harp_fcst} object to make a scatter
+#'   plot for - must not be quoted.
+#' @param parameter The parameter to plot - used to identify the observations
+#'   column in \code{.fcst}. Must not be quoted.
+#' @param members The member(s) to include in the forecast statistics if
+#'   \code{.fcst} is for EPS forecasts. Must be either a numeric vector of
+#'   member numbers or "all" to include all members.
+#' @param binwidth A 2 element numeric vector giving the width of the bins to
+#'   plot for the observed and forecast directions respectively. Set to NULL
+#'   (the default) to use the deafult of the width that gives 30 bins.
+#' @param colours A chracter vector of colours used to generate a colour
+#'   gradient.
+#' @param ... Options for \link[ggplot2]{scale_colour_gradientn}
+#'
+#' @return A hexbin plot.
+#' @export
+#'
+#' @examples
+plot_scatter <- function(.fcst, fcst_model, parameter, members = "all", binwidth = NULL, colours = NULL, ...) {
+
+  fcst_model <- rlang::quo_name(rlang::enquo(fcst_model))
+
+  if (!fcst_model %in% names(.fcst)) {
+    stop (fcst_model, " not found in .fcst.", call. = FALSE)
+  }
+
+  parameter_quo  <- rlang::enquo(parameter)
+  parameter_name <- rlang::quo_name(parameter_quo)
+
+  plot_data <- .fcst[[fcst_model]]
+
+  if (!parameter_name %in% names(plot_data)) {
+    stop (parameter_name, " observations not found in .fcst[['", fcst_model, "']]", call. = FALSE)
+  }
+
+  if (any(grepl("_mbr", names(plot_data)))) {
+    attr(plot_data, "dataframe_format") <- "wide"
+    plot_data <- harpPoint::gather_members(plot_data)
+    if (is.numeric(member)) {
+      members   <- paste0("mbr", formatC(member, width = 3, flag = "0"))
+      plot_data <- dplyr::filter(plot_data, .data$member %in% members)
+    } else if (member != "all") {
+      stop("'member' must be a numeric vector or 'all'.", call. = FALSE)
+    }
+  } else {
+    if (any(grepl("_det", names(plot_data)))) {
+      det_names <- names(plot_data)[grep("_det", names(plot_data))]
+      if (length(det_names) > 1) {
+        stop("Yo! We ain't having you have more than 1 deterministic model, fool!", call. = FALSE)
+      }
+      plot_data <- dplyr::rename(plot_data, forecast = .data$det_names)
+    }
+  }
+
+  plot_data  <- dplyr::rename(plot_data, observed = !! parameter_quo)
+  data_range <- range(c(plot_data$forecast, plot_data$observed))
+
+  x_axis     <- rlang::sym("observed")
+  y_axis     <- rlang::sym("forecast")
+
+  gg <- ggplot2::ggplot(plot_data, ggplot2::aes(!! x_axis, !! y_axis)) +
+    ggplot2::geom_hex(binwidth = binwidth) +
+    ggplot2::geom_abline(slope = 1, intercept = 0, colour = "grey50") +
+    ggplot2::coord_fixed(1, data_range, data_range) +
+    ggplot2::theme_bw()
+
+  if (!is.null(colours)) {
+    gg <- gg + ggplot2::scale_fill_gradientn(colours = colours, ...)
+  } else {
+    gg <- gg + ggplot2::scale_fill_viridis_c(option = "C")
+  }
+
+  gg
+
+}
