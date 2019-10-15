514d081a122438be55364efb43a748420aaa1cf5
andrew <36473866+andrew-MET@users.noreply.github.com>
2019-10-15 21:12:26 +0000

    Do previous commit properly

--- R/plot_point_verif.R
+++ R/plot_point_verif.R
@@ -543,7 +543,7 @@ plot_point_verif <- function(
   if (tolower(colour_by_name == "none")) {
     gg <- ggplot2::ggplot(plot_data, ggplot2::aes(!! x_axis_quo, !! y_axis_quo, ...))
   } else {
-    gg <- ggplot2::ggplot(plot_data, ggplot2::aes(!! x_axis_quo, !! y_axis_quo, colour = !! colour_by_quo, fill = !! colour_by_quo))
+    gg <- ggplot2::ggplot(plot_data, ggplot2::aes(!! x_axis_quo, !! y_axis_quo, colour = !! colour_by_quo, fill = !! colour_by_quo, ...))
   }
 
   if (is.function(colour_theme)) {
