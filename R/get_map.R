eda8db8453cba8c1bee753121fb920e8dc0a4d93
Andrew Singleton <andrewts@met.no>
2023-07-19 14:39:22 +0200

    get_domain now in harpCore

--- R/get_map.R
+++ R/get_map.R
@@ -44,52 +44,52 @@ get_map <- function(map = "world", dom = NULL, poly = TRUE, ...) {
 
 }
 
-#' Get domain information from a harp object
+# ' Get domain information from a harp object
+# '
+# ' @param x A harp object - can be a geofield, geolist, harp spatial data frame,
+# '   harp analysis or harp forecast
+# ' @param col The column from which to extract the domain for harp objects that
+# '   include data frames
+# '
+# ' @return An object (or list of objects) of class "geodomain"
+# ' @export
+# '
+# ' @examples
+#' get_domain <- function(x, ...) {
+#'   UseMethod("get_domain")
+#' }
 #'
-#' @param x A harp object - can be a geofield, geolist, harp spatial data frame,
-#'   harp analysis or harp forecast
-#' @param col The column from which to extract the domain for harp objects that
-#'   include data frames
+#' #' @export
+#' get_domain.geofield <- function(x) {
+#'   meteogrid::as.geodomain(x)
+#' }
 #'
-#' @return An object (or list of objects) of class "geodomain"
-#' @export
+#' #' @export
+#' get_domain.geolist <- function(x) {
+#'   get_domain(x[[1]])
+#' }
 #'
-#' @examples
-get_domain <- function(x, ...) {
-  UseMethod("get_domain")
-}
-
-#' @export
-get_domain.geofield <- function(x) {
-  meteogrid::as.geodomain(x)
-}
-
-#' @export
-get_domain.geolist <- function(x) {
-  get_domain(x[[1]])
-}
-
-#' @export
-get_domain.harp_spatial_fcst <- function(x, col) {
-  get_domain(dplyr::pull(x, {{col}}))
-}
-
-#' @export
-get_domain.harp_analysis <- function(x, col) {
-  res <- lapply(x, get_domain, col = {{col}})
-  names(res) <- names(x)
-  if (length(res) < 2) {
-    res <- res[[1]]
-  }
-  res
-}
-
-#' @export
-get_domain.harp_fcst <- function(x, col) {
-  res <- lapply(x, get_domain, col = {{col}})
-  names(res) <- names(x)
-  if (length(res) < 2) {
-    res <- res[[1]]
-  }
-  res
-}
+#' #' @export
+#' get_domain.harp_spatial_fcst <- function(x, col) {
+#'   get_domain(dplyr::pull(x, {{col}}))
+#' }
+#'
+#' #' @export
+#' get_domain.harp_analysis <- function(x, col) {
+#'   res <- lapply(x, get_domain, col = {{col}})
+#'   names(res) <- names(x)
+#'   if (length(res) < 2) {
+#'     res <- res[[1]]
+#'   }
+#'   res
+#' }
+#'
+#' #' @export
+#' get_domain.harp_fcst <- function(x, col) {
+#'   res <- lapply(x, get_domain, col = {{col}})
+#'   names(res) <- names(x)
+#'   if (length(res) < 2) {
+#'     res <- res[[1]]
+#'   }
+#'   res
+#' }
