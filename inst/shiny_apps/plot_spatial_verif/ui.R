de8f7167258e58ab1010b9738e27ab0b041f5c01
Ahto Mets <ahto.mets@envir.ee>
2024-02-23 10:33:51 +0000

    modified ui and server

--- inst/shiny_apps/plot_spatial_verif/ui.R
+++ inst/shiny_apps/plot_spatial_verif/ui.R
@@ -1,86 +1,117 @@
-library(shiny)
-# global.R hasn't been read yet, so you need it here:
-source("settings/set_parameters.R")
-# TODO: lead time!
-shinyUI(
-  fluidPage(
-    tags$head(tags$script(src = "message-handler.js")),
-    headerPanel("HARP spatial verification"),
-    sidebarLayout(
-        sidebarPanel(
-# SQLITE FILE
-         fluidRow(
-           column(10,textInput("sqlfile","SQLite file:", value=NULL,
-                  placeholder="/path/to/file.sqlite")),
-           column(1,actionButton("SQLbutton","OK"))
-         ),
-        tags$style(type='text/css', "#SQLbutton { margin-top: 25px;}"),
-# DATE SETTINGS:
-         fluidRow( 
-            column(4, selectInput("vPeriod","Period",
-                      choices = c("Monthly"="month","Season"="season","Recent"="recent","Custom"="custom"),
-                      multiple=FALSE,selected="custom")),
-            column(5, selectInput("fct","Run time:",choices=c(0,6,12,18),multiple=TRUE,selected=0 ))),
-         conditionalPanel( condition="input.vPeriod=='month' || input.vPeriod=='season' ", fluidRow(
-           column(4, selectInput("vYear","Year:",choices=yearMIN:yearMAX,multiple=FALSE,selected=yearMAX)),
-           conditionalPanel(condition="input.vPeriod=='month'",
-             column(4, selectInput("vMonth","Month:",choices=1:12,multiple=FALSE,selected=1)) ),
-           conditionalPanel( condition="input.vPeriod=='season'",
-             column(6, selectInput("vSeason","Season:",
-                           choices=c("Winter (DJF)"="winter","Spring (MAM)"="spring",
-                                    "Summer (JJA)"="summer","Autumn (SON)"="autumn"),
-                           multiple=FALSE,selected="winter")) ))),
-                     ## the "custom" date must have limits adapted to the data
-                     ## so it's 
-         conditionalPanel( condition="input.vPeriod=='custom'",
-            uiOutput("UI_daterange")
-#            dateRangeInput( "vDateRange","Date range:", format="yyyy-mm-dd",
-#                            min=dateMIN,max=dateMAX,start="2016-01-01",end="2016-01-31")
-         ),
-# PARAMETER & MODEL SELECTION
-         fluidRow(
-           column(4,uiOutput("UI_prmlist")),
-           column(4,uiOutput("UI_ldtlist")),
-           column(4,uiOutput("UI_modellist"))
-         ),
-# SCORE dependent
-         conditionalPanel( condition="input.tab1 == 'fuzzy'",
-           fluidRow(
-             column(6, selectInput("fuzzyScore","Score",choices=scoreList_fuzzy)),
-             column(6, selectInput("fuzzyStyle","Style",choices=styleList_fuzzy))
-           )
-         ),
-#         conditionalPanel( condition="input.tab1 == 'SAL'",
-#           fluidRow(
-#             column(6, selectInput("salStyle","Style",choices=styleList_sal))
-#           )
-#         ),
-         conditionalPanel( condition="input.tab1 == 'basic'",
-           fluidRow(
-             column(6, selectInput("basicScore","Score",choices=scoreList_basic)),
-             column(6, selectInput("basicStyle","Score",choices=styleList_basic))
-           )
-         )
-       ), # sidebarPanel
-        mainPanel(
-          tabsetPanel(
-#            tabPanel("basic", plotOutput("plotBasic")),
-            tabPanel("fuzzy", plotOutput("plotFuzzy")),
-### unfortunately, the following doesn't work at all:
-#              conditionalPanel( condition="input.fuzzyStyle == 'table' && input.tab1 == 'fuzzy'",
-#                                dataTableOutput("DataTable")),
-#              conditionalPanel( condition="input.fuzzyStyle == 'color' && input.tab1 == 'fuzzy'",
-#                                plotOutput("plotFuzzy"))),
-            tabPanel("SAL", plotOutput("plotSAL")),
-#              conditionalPanel( condition="input.salStyle == 'table' && input.tab1 == 'SAL'",
-#                                dataTableOutput("DataTable")),
-#              conditionalPanel( condition="input.salStyle == 'plot' && input.tab1 == 'SAL'",
-#                                plotOutput("plotSAL"))),
-            tabPanel("fuzzyData",dataTableOutput("DataTable")),
-            id="tab1",selected="SAL"
-          )
-        ) # mainPanel
-     ) #sidebarLayout
-  ) #fluidPage
-) # shinyUI
+# UI for shiny_plot_spatial_verif
 
+font_link <- shiny::tags$link("")
+is_online <- shiny::getShinyOption("online")
+if (is.null(is_online)) {
+  is_online <- TRUE
+}
+hostname  <- Sys.getenv("HOSTNAME")
+if (is_online & (!is.null(hostname) && !grepl("^ecgb", hostname))) {
+  font_link <- shiny::tags$link(
+    href="https://fonts.googleapis.com/css?family=Comfortaa:400,700",  rel="stylesheet"
+  )
+}
+css_file <- switch(
+  shiny::getShinyOption("theme"),
+  "light" = "harp_light.css",
+  "dark"  = "harp_midnight.css",
+  "white" = "harp_white.css"
+)
+
+
+ui <- shiny::tags$html(
+  shiny::tags$head(
+    font_link#,
+    # shiny::tags$script('
+    #   var dimension = [0, 0];
+    #   $(document).on("shiny:connected", function(e) {
+    #     dimension[0] = window.innerWidth;
+    #     dimension[1] = window.innerHeight;
+    #     Shiny.onInputChange("dimension", dimension);
+    #   });
+    #   $(window).resize(function(e) {
+    #     dimension[0] = window.innerWidth;
+    #     dimension[1] = window.innerHeight;
+    #     Shiny.onInputChange("dimension", dimension);
+    #   });
+    # ')
+  ),
+  shiny::tags$body(
+    shiny::tags$div(
+      class = "harp_page_header",
+      shiny::span(class = "harp_page_title", "harp : : Spatial Verification"),
+      shiny::div(class = "harp_logo",
+                 shiny::img(src = "harp_logo_dark.svg", height = "70px")
+      )
+    ),
+
+  shiny::fluidPage(
+
+      # App title ----
+      title = "harp",
+
+      shiny::includeCSS(css_file),
+
+      #harpVis::options_barUI("options_bar"),
+
+      #shiny::fluidRow(
+      #  harpVis::group_selectorsUI("group_selectors")
+      #),
+
+      #shiny::fluidRow(
+      #  harpVis::time_axisUI("time_axis"),
+      #  harpVis::colour_choicesUI("colour_choices")
+      #),
+
+      # Sidebar layout with input and output definitions ----
+      sidebarLayout(
+
+        # Sidebar panel for inputs ----
+        sidebarPanel(width = 3,
+
+			# Input: Select a file ----
+			fileInput("filein", "Choose file (sqlite)", multiple = FALSE, accept = c(".sqlite")),
+            
+            conditionalPanel(
+            condition = "output.fileUploaded != 0",
+                # Input: Select score
+                selectInput("score", "Select score",
+                              choices = list("NA" = 1),
+                                        selected = 1),
+
+                # Input: Select a daterange
+                dateRangeInput("dates","Date range"),
+
+                # Input: Select model
+                selectInput("model", "Select model(s)",
+                              choices = list("NA" = 1),
+                                        selected = 1),
+
+                # Input: Select leadtimes
+                selectInput("leadtime", "Select leadtimes (hours)",
+                              choices = list("NA" = 1),
+                                        selected = 1,
+                                        multiple = TRUE),
+
+                # Input: Select param
+                selectInput("param", "Select parameter",
+                              choices = list("NA" = 1),
+                                        selected = 1),
+
+                # Input: Plot/Show data using selections above
+                actionButton("showdata","Show Data")
+            )
+        ),
+		
+        # Main panel for displaying outputs ----
+        mainPanel(
+        # Output: Data file ----
+          tabsetPanel(id = "tab_panel",
+                        tabPanel("Plot", plotOutput("plot")),
+                        tabPanel("Table", DT::dataTableOutput("table")),
+          )
+        ) # end of main panel
+      ) # end of sidebar layout
+    ) # end of fluid page
+  ) # end of tags$body
+) # end of tags$html
